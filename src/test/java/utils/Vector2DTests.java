6392cafb4e75e2ef5ae47d57a59c9ab961499c5c
lkell <leokello@gmail.com>
2018-12-19 00:07:40 -0700

    Added Vector2D tests

--- src/test/java/utils/Vector2DTests.java
+++ src/test/java/utils/Vector2DTests.java
@@ -20,16 +20,33 @@ public class Vector2DTests {
 
     @Test
     public void testScalarMultiplication() {
-        Vector2D vec = new Vector2D(1, 10);
-        Vector2D newVec = vec.multiply(.1);
-        Assertions.assertEquals(.1, newVec.getX());
-        Assertions.assertEquals(1, newVec.getY());
+        Assertions.assertEquals(new Vector2D(1, 10).multiply(.1), new Vector2D(.1, 1));
     }
 
     @Test
     public void testVectorMultiplication() {
         Vector2D vec1 = new Vector2D(1, 2);
-        Vector2D vec2 = new Vector2D(2, 4);
+        Vector2D vec2 = new Vector2D(2, 3);
         Assertions.assertEquals(10, vec1.dot(vec2));
+    }
+
+    @Test
+    public void testEquals() {
+        Assertions.assertEquals(new Vector2D(1, 1), new Vector2D(1, 1));
+    }
+
+    @Test
+    public void testScalarDivision() {
+        Assertions.assertEquals(new Vector2D(10, 20).divide(5), new Vector2D(2, 4));
+    }
+
+    @Test
+    public void testNorm() {
+        Assertions.assertEquals(new Vector2D(3, 4).norm(), 5);
+    }
+
+    @Test
+    public void testMidpoint() {
+        Assertions.assertEquals(new Vector2D(0, 0).midpoint(new Vector2D(10, 10)), new Vector2D(5, 5));
     }
 }
